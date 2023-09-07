6fd4b0dd70b753fa77056ffd25510b61ba72893b
NicolasTanure <nicolastanurem@hotmail.com>
2023-09-06 21:05:41 -0300

    tools

---
+++ Tools/tools.js
@@ -0,0 +1,4 @@
+console.log(`Nome do arquivo: `, __filename)
+console.log(`diretorio do arquivo: `, __dirname)
+console.log(`Parametro de execução: `,process.argv)
+console.log(`ambiente do servidor `, process.platform)
